1e1dd4d2f9a2ca6095464ae15c12c75544fbdc18
Napoleon Romano <romanoyaco@gmail.com>
2023-05-10 03:47:24 -0300

    cambios finales 2

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
 'use strict'
 
-const grande    = document.querySelector('.grande')
-const punto     = document.querySelectorAll('.punto')
+const grande = document.querySelector('.grande')
+const punto = document.querySelectorAll('.punto')
 
 // Cuando CLICK en punto
-    // Saber la posición de ese punto
-    // Aplicar un transform translateX al grande
-    // QUITAR la clase activo de TODOS puntos
-    // AÑADIR la clase activo al punto que hemos hecho CLICK
+// Saber la posición de ese punto
+// Aplicar un transform translateX al grande
+// QUITAR la clase activo de TODOS puntos
+// AÑADIR la clase activo al punto que hemos hecho CLICK
 
 // Recorrer TODOS los punto
-punto.forEach( ( cadaPunto , i )=> {
+punto.forEach((cadaPunto, i) => {
     // Asignamos un CLICK a cadaPunto
-    punto[i].addEventListener('click',()=>{
+    punto[i].addEventListener('click', () => {
 
         // Guardar la posición de ese PUNTO
-        let posicion  = i
+        let posicion = i
         // Calculando el espacio que debe DESPLAZARSE el GRANDE
         let operacion = posicion * -50
 
         // MOVEMOS el grand
-        grande.style.transform = `translateX(${ operacion }%)`
+        grande.style.transform = `translateX(${operacion}%)`
 
         // Recorremos TODOS los punto
-        punto.forEach( ( cadaPunto , i )=>{
+        punto.forEach((cadaPunto, i) => {
             // Quitamos la clase ACTIVO a TODOS los punto
             punto[i].classList.remove('activo')
         })
@@ -32,3 +32,46 @@ punto.forEach( ( cadaPunto , i )=> {
 
     })
 })
+
+$(document).ready(function () {
+    $('#btnEnviar').on('click', function (e) {
+        e.preventDefault(); // Evita que se envíe el formulario de manera predeterminada
+
+        // Obtenemos los valores del formulario
+        var nombre = $('input[name="fullname"]').val();
+        var email = $('input[name="email"]').val();
+        var telefono = $('input[name="phone"]').val();
+        var asunto = $('input[name="affair"]').val();
+        var mensaje = $('textarea[name="message"]').val();
+
+        // Configuramos el correo electrónico que se enviará
+        var destinatario = 'romanoyaco@gmail.com'; // Cambia esto por tu dirección de correo electrónico
+        var asuntoCorreo = 'Mensaje de contacto desde tu sitio web';
+        var cuerpoCorreo = 'Nombre: ' + nombre + '\n' +
+            'Email: ' + email + '\n' +
+            'Teléfono: ' + telefono + '\n' +
+            'Asunto: ' + asunto + '\n' +
+            'Mensaje: ' + mensaje;
+
+        // Enviamos el correo electrónico a través de una petición AJAX
+        $.ajax({
+            url: 'https://smtpjs.com/v3/smtp.js',
+            type: 'POST',
+            data: {
+                to: destinatario,
+                from: email,
+                subject: asuntoCorreo,
+                body: cuerpoCorreo
+            },
+            success: function () {
+                alert('El correo electrónico se ha enviado correctamente');
+            },
+            error: function (xhr, status, error) {
+                console.log(xhr);
+                console.log(status);
+                console.log(error);
+                alert('Hubo un error al enviar el correo electrónico. Por favor, inténtelo de nuevo más tarde');
+            }
+        });
+    });
+});
